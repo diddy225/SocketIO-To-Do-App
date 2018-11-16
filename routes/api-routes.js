d272cb78fea6f3d564313540f7d7ed4fdb23b89e
Matthew C <matthew.j.carpenter1@gmail.com>
2018-11-16 00:34:31 -0500

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,13 +3,13 @@ const RestfulAPI = require('./restclass');
 
 module.exports = function (app) {
 
-const getItems = new RestfulAPI('/api/todo', app, db.Todo)
+const getItems = new RestfulAPI('api/todo', app, db.Todo)
 getItems.findAll();
 
-const complete = new RestfulAPI('/api/todo', app, db.Todo)
+const complete = new RestfulAPI('api/todo', app, db.Todo)
 complete.update();
 
-const removeTodo = new RestfulAPI('/api/todo', app, db.Todo)
+const removeTodo = new RestfulAPI('api/todo', app, db.Todo)
 removeTodo.delete();
 
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const http = require('http');
 
+const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// mongoose.connect('mongodb://localhost/realtimetodo', { useNewUrlParser: true});
-mongoose.connect('mongodb://user:pass12@ds041377.mlab.com:41377/heroku_wzvb04pg', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/realtimetodo', { useNewUrlParser: true});
+// mongoose.connect('mongodb://user:pass12@ds041377.mlab.com:41377/heroku_wzvb04pg', { useNewUrlParser: true});
 
 require('./sockets/todo-sockets.js')(io);
 require('./routes/html-routes.js')(app);
