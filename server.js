a5a40835735b534adc0ea5fd4735de152f9f7a6d
Matthew C <matthew.j.carpenter1@gmail.com>
2018-11-16 00:35:02 -0500

    updated routes

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/realtimetodo', { useNewUrlParser: true});
-// mongoose.connect('mongodb://user:pass12@ds041377.mlab.com:41377/heroku_wzvb04pg', { useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost/realtimetodo', { useNewUrlParser: true});
+mongoose.connect('mongodb://user:pass12@ds041377.mlab.com:41377/heroku_wzvb04pg', { useNewUrlParser: true});
 
 require('./sockets/todo-sockets.js')(io);
 require('./routes/html-routes.js')(app);
